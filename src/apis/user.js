b9ba7d90d8de3395a1da0b6e20501e3ab2a80b3d
siam-craft <abdullahsiam009@gmail.com>
2023-12-21 18:29:07 +0600

    user is adding into the database

--- src/apis/user.js
+++ src/apis/user.js
@@ -4,8 +4,8 @@ import {
   AuthenticateValidators,
   RegisterValidators,
 } from "../validators/index.js";
-import { validationResult } from "express-validator";
 import Validator from "../middlewares/validatorMiddleware.js";
+import { randomBytes } from "crypto";
 
 const router = Router();
 
@@ -23,7 +23,28 @@ router.post(
   async (req, res) => {
     const { username, email } = req.body;
     // check if the user name is taken or not
-    const user = await User.findOne({ username });
+    let user = await User.findOne({ username });
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Username is taken" });
+    }
+
+    // check if the user exists with that email
+    user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ success: false, error: "Email is taken" });
+    }
+
+    user = new User({
+      ...req.body,
+      verificationCode: randomBytes(20).toString("hex"),
+    });
+    const result = await user.save();
+
+    return res.status(200).json({ success: true, data: result });
+
+    // send the email to the user
   },
 );
 
