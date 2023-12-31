ba8e3db9a264782a443b93402fa6f083777ec268
siam-craft <abdullahsiam009@gmail.com>
2023-12-31 17:23:56 +0600

    profile routes

--- src/apis/profiles.js
+++ src/apis/profiles.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { userAuth } from "../middlewares/auth-guard.js";
 import uploader from "../middlewares/uploader.js";
 import { DOMAIN } from "../constants/index.js";
-import { Profile } from "../models/index.js";
+import { Profile, User } from "../models/index.js";
 
 const router = Router();
 
@@ -75,5 +75,65 @@ router.get("/api/my-profile", userAuth, async (req, res) => {
  * @access Private
  * @type GET <multipart form request>
  */
+
+router.put(
+  "/api/update-profile",
+  userAuth,
+  uploader.single("avatar"),
+  async (req, res) => {
+    try {
+      let { body, file, user } = req;
+      let path = DOMAIN + file.path.split("uploads/")[1];
+      let profile = await Profile.findOneAndUpdate(
+        { account: user._id },
+        { social: body, avatar: path },
+        {
+          new: true,
+        },
+      );
+      return res.status(200).json({
+        success: true,
+        message: "Profile updated successfully",
+        profile,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        success: false,
+        message: "Unable to update profile",
+      });
+    }
+  },
+);
+
+/**
+ * @description To get public profile info
+ * @api /api/profile-user/:username
+ * @access Public
+ * @type GET
+ */
+
+router.get("/api/profile-user/:username", async (req, res) => {
+  try {
+    let { username } = req.params;
+    let user = await User.findOne({ username });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Profile not found" });
+    }
+    let profile = await Profile.findOne({ account: user._id });
+    return res.status(200).json({
+      success: true,
+      profile: {
+        ...profile.toObject(),
+        account: user.getUserInfo(),
+      },
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Unable to get the profile" });
+  }
+});
 
 export default router;
