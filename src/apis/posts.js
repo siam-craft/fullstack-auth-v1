151abd47e85a06ab06aeabc5e7c3b2292ed167ec
siam-craft <abdullahsiam009@gmail.com>
2024-01-04 17:06:23 +0600

    post create and update by author

--- src/apis/posts.js
+++ src/apis/posts.js
@@ -6,6 +6,7 @@ import { userAuth } from "../middlewares/auth-guard.js";
 import { PostValidator } from "../validators/index.js";
 import validator from "../middlewares/validatorMiddleware.js";
 import { uploadPostImage as uploader } from "../middlewares/uploader.js";
+import slugGenerator from "../functions/slugGenerator.js";
 
 const router = Router();
 
@@ -24,10 +25,12 @@ router.post(
     try {
       let { file } = req;
       console.log(file, "file");
-      let path = DOMAIN + file.path.split("uploads/")[1];
-      return res
-        .status(200)
-        .json({ success: true, message: "Image upload successfully" });
+      let filename = DOMAIN + file.path.split("uploads/")[1];
+      return res.status(200).json({
+        filename,
+        success: true,
+        message: "Image upload successfully",
+      });
     } catch (error) {
       return res
         .status(400)
@@ -51,10 +54,64 @@ router.post(
   async (req, res) => {
     try {
       // Create a new post
+      let { body } = req;
+      let post = new Post({
+        author: req.user._id,
+        ...body,
+        slug: slugGenerator(body.title),
+      });
+      await post.save();
+      return res.status(201).json({
+        post,
+        message: "Post created successfully",
+        success: true,
+      });
     } catch (error) {
       return res
         .status(400)
         .json({ message: "Unable to create post", success: false });
+    }
+  },
+);
+
+/**
+ * @description To create a new post by the authenticated user
+ * @api /posts/api/create-post
+ * @access Private
+ * @type POST
+ */
+
+router.put(
+  "/api/update-post/:id",
+  userAuth,
+  PostValidator,
+  validator,
+  async (req, res) => {
+    try {
+      let { id } = req.params;
+      let { body, user } = req;
+      console.log(user, "user");
+      // check if the post with the id is in the database or not?
+      let post = await Post.findById(id);
+      if (post.author.toString() !== user._id.toString()) {
+        return res
+          .status(401)
+          .json({ success: false, message: "Post doesn't belong to you" });
+      }
+      post = await Post.findByIdAndUpdate(
+        { author: user._id, _id: id },
+        { ...body, slug: slugGenerator(body.title) },
+        { new: true },
+      );
+
+      return res
+        .status(200)
+        .json({ message: "Post updated successfully", post, success: true });
+    } catch (error) {
+      return res.status(400).json({
+        success: false,
+        message: "Unable to update post",
+      });
     }
   },
 );
--- src/models/Post.js
+++ src/models/Post.js
@@ -7,6 +7,10 @@ const PostSchema = new Schema(
       type: String,
       required: true,
     },
+    slug: {
+      type: String,
+      required: true,
+    },
     title: {
       type: String,
       required: true,
