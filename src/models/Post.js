00bc434e1257f832b4237d40a5b851bfaa92cd26
siam-craft <abdullahsiam009@gmail.com>
2024-01-04 14:00:11 +0600

    Post schema added

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,9 +1,46 @@
 import { Schema, model } from "mongoose";
 import paginator from "mongoose-paginate-v2";
 
-const PostSchema = new Schema({}, { timestamps: true });
+const PostSchema = new Schema(
+  {
+    postImage: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    comments: [
+      {
+        text: {
+          type: String,
+          required: true,
+        },
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+      },
+    ],
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true },
+);
 
 PostSchema.plugin(paginator);
 
-const Post = model("Post", PostSchema);
+const Post = model("posts", PostSchema);
 export default Post;
--- src/models/index.js
+++ src/models/index.js
@@ -1,2 +1,4 @@
 export { default as User } from "./User.js";
 export { default as Profile } from "./Profile.js";
+export { default as Post } from "./Post.js";
+
